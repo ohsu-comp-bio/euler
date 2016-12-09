2addd719cf25b0db6901f00569a93aac57b88273
Brian Walsh <brian@bwalsh.com>
2016-12-08 16:10:41 -0800

    initial checkin

---
+++ services/api/settings.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import os
+import bioschemas
+
+MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
+MONGO_PORT = os.environ.get('MONGO_PORT', 27017)
+MONGO_USERNAME = os.environ.get('MONGO_USERNAME', '')
+MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', '')
+MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'test')
+
+DEBUG = 'API_DEBUG' in os.environ
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+# Enable reads (GET), edits (PATCH) and deletes of individual items
+# (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'DELETE']
+
+# We enable standard client cache directives for all resources exposed by the
+# API. We can always override these global settings later.
+CACHE_CONTROL = 'max-age=20'
+CACHE_EXPIRES = 20
+
+# make query parameters conform to GDC
+# https://gdc-docs.nci.nih.gov/API/Users_Guide/Search_and_Retrieval/#components-of-a-request
+# Default value for QUERY_MAX_RESULTS. Defaults to 25.
+PAGINATION_DEFAULT = 10
+# Key for the filters query parameter. Defaults to where.
+QUERY_WHERE = 'filters'
+# Key for the projections query parameter. Defaults to projection.
+QUERY_PROJECTION = 'fields'
+
+
+# not used
+HATEOAS = False
+
+# conform with GDC
+API_VERSION = 'v0'
+
+# important - we don't need to overly specify schemas
+ALLOW_UNKNOWN = True
+
+"""
+Notes:
+* Essentially a URI
+* C(R)UD is routed to mongo collection 'file'
+* (R)Search is routed to flask route aggregated_resource (no 'GET')
+"""
+file = {
+    'description':
+        """
+        A set of related records (either written or electronic) kept together.
+        (NCIt C42883).
+        """,
+    'schema': bioschemas.cerberus_schema('Resource'),
+    'datasource': {
+        'source': 'file',
+    },
+    'resource_methods': ['POST', 'DELETE']
+}
+
+# TODO Remove after adding token to tests
+PUBLIC_METHODS = ['GET', 'POST', 'PATCH', 'DELETE']
+PUBLIC_ITEM_METHODS = ['GET', 'POST', 'PATCH', 'DELETE']
+
+#
+SCHEMA_ENDPOINT = 'schemas'
+
+# not used
+XML = False
+
+# The DOMAIN dict explains which resources will be available and how they will
+# be accessible to the API consumer.
+DOMAIN = {
+    'files': file,
+}
---
+++ services/api/tests/integration/keystone_authenticator_tests.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+"""
+Test authenticator endpoints, depends on keystone
+"""
+from keystone_authenticator import BearerAuth
+from json import dumps
+import os
+
+
+def test_encode_decode_token():
+    profile = {'foo': 'bar'}
+    auth = BearerAuth()
+    token = auth.make_token(profile)
+    parsed = auth.parse_token(token)
+    assert parsed == profile
+
+
+def test_authenticate(client):
+    auth = BearerAuth()
+    token = auth.authenticate_user(
+        username=os.environ.get('OS_USERNAME'),
+        user_domain_name=os.environ.get('OS_USER_DOMAIN_NAME'),
+        password=os.environ.get('OS_PASSWORD')
+    )
+    profile = auth.parse_token(token)
+    assert profile
+    assert profile['name']
+    assert profile['domain_name']
+    assert profile['mail']
+    assert profile['token']
+    assert len(profile['roles']) > 0
+    for role in profile['roles']:
+        assert role['role']
+        assert role['scope']
+        assert role['scope']['project']
+        assert role['scope']['domain']
+
+
+def test_login(client, app):
+    """
+    should respond with ok and user
+    """
+    _development_login(client, app)
+
+
+def test_logout(client):
+    """
+    should respond with ok and user
+    """
+    r = client.post('/v0/logout')
+
+
+def _development_login(client, app):
+    # save current auth, and ensure ohsu_authenticator used for this test
+    old_auth = app.auth
+    app.auth = BearerAuth()
+    r = client.post('/v0/login',
+                    data=dumps({"domain":
+                                os.environ.get('OS_USER_DOMAIN_NAME'),
+                                "user": os.environ.get('OS_USERNAME'),
+                                "password": os.environ.get('OS_PASSWORD')
+                                }),
+                    content_type='application/json')
+    app.auth = old_auth
+    assert r.status_code == 200
+    assert r.json['id_token']
+    return r.json['id_token']
+
+
+def test_project_lookup(client, app):
+    auth = BearerAuth()
+    token = {u'mail': u'None', u'token': u'foo', u'domain_name': u'Default',
+             u'roles': [
+              {u'scope': {u'project': u'admin', u'domain': u'Default'}, u'role': u'admin'},  # NOQA
+              {u'scope': {u'project': u'user', u'domain': u'Default'}, u'role': u'member'},  # NOQA
+              ], u'name': u'admin'}
+    assert len(auth._find_projects(token)) == 2
+
+
+def test_should_retrieve_projects(client, app):
+    auth = BearerAuth()
+    len(auth.projects(_development_login(client, app))) == 1
+
+
+def test_check_auth(client, app):
+    auth = BearerAuth()
+    # for now, only returns true
+    assert auth.check_auth(None, None, None, None)
+
+
+def test_check_default_projects(client, app):
+    auth = BearerAuth()
+    # for now, no public projects
+    assert len(auth.projects(None, None)) == 0
+
+
+def test_bad_login(client, app):
+    """
+    should respond with ok and user
+    """
+    old_auth = app.auth
+    app.auth = BearerAuth()
+    r = client.post('/v0/login', headers={'content-type': 'application/json'},
+                    data=dumps({'user': 'FOO', 'password': 'password'}))
+    app.auth = old_auth
+    assert r.status_code == 401
+
+
+def test_projects_from_token(client, app):
+    id_token = _development_login(client, app)
+    # save current auth, and ensure test_authenticator used for this test
+    old_auth = app.auth
+    app.auth = BearerAuth()
+    request = {'headers': []}
+    request = MockRequest()
+    request.headers['authorization'] = 'Bearer {}'.format(id_token)
+    projects = app.auth.projects(request=request)
+    user = app.auth.get_user(request=request)
+    app.auth = old_auth
+    assert len(projects) > 0
+    assert user
+
+
+def test_projects_from_unauthorized_token(client, app):
+    # save current auth, and ensure test_authenticator used for this test
+    old_auth = app.auth
+    app.auth = BearerAuth()
+    request = {'headers': []}
+
+    class MockRequest:
+        headers = {}
+
+    request = MockRequest()
+    projects = app.auth.projects(request=request)
+    app.auth = old_auth
+    assert len(projects) == 0
+
+
+class MockRequest:
+    headers = {}
